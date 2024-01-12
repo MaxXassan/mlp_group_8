111da582bd3365f034c685c1ffd7c1fcb38a2c69
MaxXassan <mohamed.abdirisak.hassan@gmail.com>
2024-01-12 19:27:28 +0100

    Updated demo.py

--- CNNClothesClassifier/frontend/demo.py
+++ CNNClothesClassifier/frontend/demo.py
@@ -3,8 +3,6 @@ import os
 import sys
 import torch
 import torchvision.transforms as T
-import numpy as np
-from PIL import Image
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
